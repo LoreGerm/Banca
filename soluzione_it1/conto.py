d69980eac97d78920ceb3ce591803ad9ef30f835
LoreGerm <lorenzogermano146@gmail.com>
2022-01-27 12:55:17 +0100

    .

--- soluzione_it1/conto.py
+++ soluzione_it1/conto.py
@@ -4,12 +4,12 @@
 
 class conto:
 
-    def __init__(self, bilancio_conto, numero_conto, saldo=0):
+    def __init__(self, numero_conto, bilancio_conto, saldo=0):
         self.__bilancio_conto = bilancio_conto
         self.__numero_conto = numero_conto
         self.__saldo = saldo
         self.__operazioni_effettuate = []
 
     def __repr__(self):
-        return f"Bilancio conto: {self.__bilancio_conto} \n {self.__numero_conto} \n Saldo: {self.__saldo} \n Operazioni effettuate: {len(self.__operazioni_effettuate)}"
+        return f"Numero conto: {self.__numero_conto} \n {self.__bilancio_conto} \n Saldo: {self.__saldo} \n Operazioni effettuate: {len(self.__operazioni_effettuate)}"
 
